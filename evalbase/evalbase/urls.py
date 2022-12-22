6b6898eea7762bcddd55d9aaa0721a68ff03960c
Ian Soboroff <isoboroff@gmail.com>
2022-12-22 16:00:35 -0500

    Whitespace cleanups in urls

--- evalbase/evalbase/urls.py
+++ evalbase/evalbase/urls.py
@@ -48,10 +48,8 @@ urlpatterns = [
     path('agreements/<str:conf>/<str:agreement>/sign', views.sign_agreement, name='sign-agreement'),
     path('conf/<str:conf>/<str:task>/submit', views.SubmitRun.as_view(), name='submit'),
     path('conf/<str:conf>/<str:task>/<int:id>/edit', views.EditSubmission.as_view(), name='edit-task'),
-
     path('run/<str:conf>/<str:task>/<str:runtag>', views.Submissions.as_view(), name='run'),
     path('run/<str:conf>/<str:task>/<str:runtag>/download', views.download, name='run-download'),
     path('run/<str:conf>/<str:task>/<str:runtag>/delete', views.DeleteSubmission.as_view(), name='run-delete'),
-    
     path('', views.HomeView.as_view(template_name='evalbase/home.html'), name='home'),
 ]
