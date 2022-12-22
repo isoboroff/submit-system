d7ab8ed16b135c02d13442561bc511099e475ee3
Ian Soboroff <isoboroff@gmail.com>
2022-12-22 14:17:49 -0500

    Moat settings

--- evalbase/evalbase/settings/defaults.py
+++ evalbase/evalbase/settings/defaults.py
@@ -24,8 +24,6 @@ BASE_DIR = Path(__file__).parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- evalbase/evalbase/settings/prod.py
+++ evalbase/evalbase/settings/prod.py
@@ -2,3 +2,4 @@ from .defaults import *
 DEBUG = False
 SECRET_KEY = 'NEED A REAL KEY HERE'
 ALLOWED_HOSTS = ['127.0.0.1']
+EMAIL_HOST = 'smtp.nist.gov'
