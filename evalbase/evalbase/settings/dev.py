8c119a2f82188a88b3c6d4f118bd2b64696c862c
Ian Soboroff <isoboroff@gmail.com>
2022-12-27 13:39:21 -0500

    Added support for more verbose logging in the dev settings

--- evalbase/evalbase/settings/dev.py
+++ evalbase/evalbase/settings/dev.py
@@ -1,3 +1,29 @@
 from .defaults import *
 DEBUG = True
 SECRET_KEY = 'b0@5@)2#*$7a&@74hn%ji2k7g511rqcvr5h&dw0iuo1xyatzx&'
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
