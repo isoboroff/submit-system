f0b2b88ac180451ecccb2bc7c155cdd084b6bc85
Ian Soboroff <isoboroff@gmail.com>
2023-01-08 15:40:16 -0500

    Adding some documentation to the decorators.

--- evalbase/evalbase/decorators.py
+++ evalbase/evalbase/decorators.py
@@ -7,6 +7,10 @@ from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 from .models import *
 
+# These permission decorators follow a general API pattern,
+# where keywords in the URLconf that are retrieved from
+# the database are returned as extra kwargs entries.
+
 def evalbase_login_required(view_func):
     '''A convenience wrapper around the Django login_required
     decorator, that specifies the arguments we use in EvalBase.
@@ -19,6 +23,8 @@ def evalbase_login_required(view_func):
 def user_is_member_of_org(view_func):
     '''Confirm that the request.user is a member of the org named
     by the kwargs 'org' and 'conf'
+    kwargs: org, conf
+    returns: _org
     '''
     @functools.wraps(view_func)
     def wrapped_view(request, *args, **kwargs):
@@ -38,6 +44,8 @@ def user_is_member_of_org(view_func):
 def user_owns_org(view_func):
     '''Confirm that the request.user is the owner of the org
     named by the kwarg 'org'.
+    kwargs: org
+    returns: _org
     '''
     @functools.wraps(view_func)
     def wrapped_view(request, *args, **kwargs):
@@ -57,6 +65,8 @@ def user_is_active_participant(view_func):
     '''Confirm that the request.user is a member of an org in the conf
     named by kwarg['conf'], and that the org has submitted at least one
     submission.
+    kwargs: conf
+    returns: _valid_orgs, _subs
     '''
     @functools.wraps(view_func)
     def wrapped_view(request, *args, **kwargs):
@@ -69,6 +79,8 @@ def user_is_active_participant(view_func):
                 .filter(task__conference__shortname=kwargs['conf'])
                 .filter(submitted_by_in=valid_orgs))
         if subs:
+            kwargs['_valid_orgs'] = valid_orgs
+            kwargs['_subs'] = subs
             return view_func(request, *args, **kwargs)
         raise PermissionDenied('User is not active participant')
     return wrapped_view
@@ -79,6 +91,8 @@ def user_is_participant(view_func):
     participate in the conf named by kwargs['conf'].  This is essentially
     the same permission as user_is_member_of_org, except there's
     no org in kwargs.
+    kwargs: conf
+    returns: _valid_orgs
     '''
     @functools.wraps(view_func)
     def wrapped_view(request, *args, **kwargs):
@@ -88,6 +102,7 @@ def user_is_participant(view_func):
                       .filter(conference__shortname=kwargs['conf'])
                       .filter(members__pk=request.user.pk))
         if valid_orgs:
+            kwargs['_valid_orgs'] = valid_orgs
             return view_func(request, *args, **kwargs)
         raise PermissionDenied('User is not a member of a participating group')
     return wrapped_view
@@ -97,6 +112,8 @@ def user_may_edit_submission(view_func):
     '''Confirm that the request.user is either the submittor
     of the kwarg 'runtag' submitted to kwarg 'conf', or that
     the request.user is the owner of the group that submitted it.
+    kwargs: conf, runtag
+    returns: _sub
     '''
     @functools.wraps(view_func)
     def wrapped_view(request, *args, **kwargs):
@@ -107,6 +124,7 @@ def user_may_edit_submission(view_func):
                                 Q(runtag=kwargs['runtag']))
         if (sub.submitted_by == request.user or
             request.user == sub.org.owner):
+            kwargs['_sub'] = sub
             return view_func(request, *args, **kwargs)
         raise PermissionDenied('User may not edit submission')
     return wrapped_view
@@ -114,6 +132,8 @@ def user_may_edit_submission(view_func):
 
 def conference_is_open(view_func):
     '''Confirm that the conference named in the kwarg 'conf' is not complete.
+    kwargs: conf
+    returns: _conf
     '''
     @functools.wraps(view_func)
     def wrapped_view(request, *args, **kwargs):
@@ -129,6 +149,8 @@ def conference_is_open(view_func):
 
 def task_is_open(view_func):
     '''Confirm that the task is still open for submissions.
+    kwargs: conf, task
+    returns: _conf, _task
     '''
     @functools.wraps(view_func)
     def wrapped_view(request, *args, **kwargs):
