34eae5eeb42548d153bfa2df44e995b0455f994a
Ian Soboroff <isoboroff@gmail.com>
2023-01-08 22:21:39 -0500

    Setting up a production environment using MySQL

--- evalbase/evalbase/settings/prod.py
+++ evalbase/evalbase/settings/prod.py
@@ -3,3 +3,12 @@ DEBUG = False
 SECRET_KEY = 'NEED A REAL KEY HERE'
 ALLOWED_HOSTS = ['127.0.0.1']
 EMAIL_HOST = 'smtp.nist.gov'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/raids/ir1/submit-system/my.cnf',
+        },
+    }
+}
