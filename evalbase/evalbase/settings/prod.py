d88ef8e352218f10197e7dd7b6398d628d23e6a7
Ian Soboroff <ian.soboroff@nist.gov>
2023-02-03 17:40:46 +0000

    Set up for productioon

--- evalbase/evalbase/settings/prod.py
+++ evalbase/evalbase/settings/prod.py
@@ -1,7 +1,7 @@
 from .defaults import *
 DEBUG = False
-SECRET_KEY = 'NEED A REAL KEY HERE'
-ALLOWED_HOSTS = ['127.0.0.1']
+SECRET_KEY = '3$cui8i8l@+r5@!qawwdf1pm0jbg!-y)mnef-9n@$!w^2(14jn'
+ALLOWED_HOSTS = ['129.6.101.59']
 EMAIL_HOST = 'smtp.nist.gov'
 
 DATABASES = {
