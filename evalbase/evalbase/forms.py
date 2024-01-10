14ffc5a2d705309edb1c9c14e10a969a732bdb34
soboroff <ian.soboroff@nist.gov>
2024-01-10 14:20:54 -0500

    Fixed pw autocomplete field on signup

--- evalbase/evalbase/forms.py
+++ evalbase/evalbase/forms.py
@@ -2,10 +2,26 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import password_validation
 from .models import *
 
 
 class SignupForm(UserCreationForm):
+    # Stop these fields from including autocomplete attributes.
+    # NIST web scanner says no-no.
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name',
