cb2e9ba1e876543d8f97adb7a6dba789b1dfaa05
George Awad <gawad@nist.gov>
2024-06-20 15:36:53 -0400

    Added AVS checker files and test cases

---
+++ checker-tests/avs.checkers/progress/run.all.sh
@@ -0,0 +1,9 @@
+perl ../tv16.check_avs.pl ./all.OK.P.videoFeatureExtractionResults.xml
+perl ../tv16.check_avs.pl ./empty.desc.P.videoFeatureExtractionResults.xml
+perl ../tv16.check_avs.pl ./empty.pid.P.videoFeatureExtractionResults.xml
+perl ../tv16.check_avs.pl ./empty.seq.P.videoFeatureExtractionResults.xml
+perl ../tv16.check_avs.pl ./empty.shotid.P.videoFeatureExtractionResults.xml
+perl ../tv16.check_avs.pl ./empty.time.P.videoFeatureExtractionResults.xml
+perl ../tv16.check_avs.pl ./wrong.shotID.P.videoFeatureExtractionResults.xml
+perl ../tv16.check_avs.pl ./wrong.topic.P.videoFeatureExtractionResults.xml
+perl ../tv16.check_avs.pl ./wrong.xml.P.videoFeatureExtractionResults.xml
---
+++ checker-tests/avs.checkers/update.topic.numbers.pl
@@ -0,0 +1,6 @@
+#! /usr/bin/perl -pi
+
+s/tv2023/tv2024/;
+
+
+
