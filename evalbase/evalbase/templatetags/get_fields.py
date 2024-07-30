fe9cfdbdb11b6d5d3e9af31b950e0b0556e26a3d
soboroff <ian.soboroff@nist.gov>
2024-07-30 07:37:46 -0400

    New template tools, not yet using, for accessing slots

--- evalbase/evalbase/templatetags/get_fields.py
+++ evalbase/evalbase/templatetags/get_fields.py
@@ -12,3 +12,18 @@ def get_field_value(obj, k):
         return k.value_from_object(obj)
     except Exception:
         return None
+
+@register.filter(name='get')
+def get(o, index):
+    try:
+        return o[index]
+    except:
+        return settings.TEMPLATE_STRING_IF_INVALID
+
+
+@register.filter(name='getattr')
+def getattrfilter(o, attr):
+    try:
+        return getattr(o, attr)
+    except:
+        return settings.TEMPLATE_STRING_IF_INVALID
