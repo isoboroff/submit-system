c29b70c861f32969c812e4115a9d870c29385a29
soboroff <ian.soboroff@nist.gov>
2024-09-27 15:50:07 -0400

    manage.py command to import evaluations

---
+++ evalbase/evalbase/management/commands/add_evals.py
@@ -0,0 +1,66 @@
+import json
+from pathlib import Path
+from django.core.management.base import BaseCommand, CommandError
+from evalbase.models import Task, Submission, Evaluation
+
+class Command(BaseCommand):
+    help = 'Import evaluation outputs for a task.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('task',
+                            help='Evals are for this task (\'list\' to list them)')
+
+        parser.add_argument('eval',
+                            help='Evaluation name',
+                            default='trec_eval')
+
+        parser.add_argument('-c', '--conf',
+                            help='Conference shortname',
+                            default='trec-2024')
+
+        parser.add_argument('-d', '--directory',
+                            help='Directory containing eval files',
+                            type=Path,
+                            default=Path('.'))
+
+        parser.add_argument('-f', '--force',
+                            help='Overwrite existing evals',
+                            action='store_true')
+
+    def handle(self, *args, **options):
+        if options['task'] == 'list':
+            tasks = (Task.objects
+                     .filter(track__conference=option['conf']))
+            for t in tasks:
+                print(t.shortname, t.longname)
+
+        else:
+            task = Task.objects.get(shortname=options['task'])
+            if not task:
+                raise CommandError(f'Task {options["task"]} not found')
+
+            eval = options['eval']
+
+            for run in Submission.objects.filter(task=task):
+                evalpath = options['dir'] / f'{run.runtag}.{eval}'
+                if not evalpath.exists():
+                    evaldir = options['dir'] / run.runtag
+                    if not (evaldir.exists() and evaldir.is_dir()):
+                        raise CommandError(f'Can\'t find {eval} eval for run {run.runtag} here or in directory {evaldir}')
+                    evalpath = evaldir / f'{run.runtag}.{eval}'
+                    if not evalpath.exists():
+                        raise CommandError(f'Can\'t find {eval} eval for run {run.runtag} in directory {evaldir}')
+
+                old_eval = Evaluation.objects.get(submission=run, name=eval)
+                if old_eval:
+                    if options['force']:
+                        old_eval.delete()
+                    else:
+                        raise CommandError(f'Found existing eval.  To overwrite, use -f')
+
+                new_eval = Evaluation()
+                new_eval.name = eval
+                new_eval.submission = run
+                with evalpath.open(mode='r') as fp:
+                    new_eval.filename = evalpath
+                    new_eval.save()
