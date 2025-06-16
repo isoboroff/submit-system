8c69fe61297c161eff26b9bc1b53db88abe526e6
soboroff <ian.soboroff@nist.gov>
2025-06-16 15:27:10 -0400

    Site is down view.

--- evalbase/evalbase/urls.py
+++ evalbase/evalbase/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -26,6 +26,7 @@ from oauth2_provider import urls as oauth2_urls
 
 
 urlpatterns = ([
+    re_path('^.*$', views.site_is_down),
     path('admin/', admin.site.urls),
     path('o/', include(oauth2_urls)),
 
